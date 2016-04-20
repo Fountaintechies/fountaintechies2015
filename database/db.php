004a3aeec628253a2062063e59c613595819fd38
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-04-20 16:04:04 +0530

    config changes

--- database/db.php
+++ database/db.php
@@ -3,15 +3,15 @@
  
 if ("localhost" == $_SERVER[ 'SERVER_NAME'] || $_SERVER[ 'SERVER_NAME'] == "127.0.0.1")
  {
-  $con = mysql_connect('localhost','root', '');
-  $web_url ="http://localhost/fountaintechies2015/";
+  //$con = mysql_connect('localhost','root', '');
+  //$web_url ="http://localhost/fountaintechies2015/";
  }
 else
  {
-  $con = mysql_connect('apps.fountaintechies.com','apps', '7gXWOqeaf');
-  $web_url ="http://fountaintechies.com";
+  //$con = mysql_connect('apps.fountaintechies.com','ftdev', '7gXWOqeaf');
+  //$web_url ="http://fountaintechies.com";
  } 
- $db  = mysql_select_db('fountaintechies');
+ //$db  = mysql_select_db('fountaintechies');
  //$con = mysql_connect('cio-choice-201408141000.ctseggpibot4.ap-southeast-1.rds.amazonaws.com','admin', 'H3ll0$c1o7#');
  
  //$db  = mysql_select_db('cio-choice_27march');
