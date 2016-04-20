68471ecfbd5740df9cbfff79c458155df554e978
sameervedpathak <sameer.vedpathak18@gmail.com>
2016-04-20 15:33:51 +0530

    home page

---
+++ index.php
@@ -0,0 +1,319 @@
+
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Home | fountaintechies</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta name="description" content="fountaintechies - We create professional Apple iOS, Google Android and HTML5 applications"/>
+        <meta name="keywords" content="fountaintechies, iOS, Android, Windows Phone, Mobile Applications, Mobile, Apps, Applications, Hybrid Applications, HTML5, Angular, AngularJS"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="About Us"/>
+        <meta property="og:url" content="http://www.fountaintechies.com/about/"/>
+        <meta property="og:site_name" content="fountaintechies"/>
+        <meta property="og:image" content="http://www.fountaintechies.com/images/af-og-image.jpg"/>
+        <meta property="og:description" content="fountaintechies - We create professional Apple iOS, Google Android and HTML5 applications"/>
+        <meta property="twitter:card" content="fountaintechies"/>
+        <meta property="twitter:site" content="@fountaintechies"/>
+        <meta property="twitter:creator" content=" @fountaintechies "/>
+        <meta property="twitter:title" content="About Us"/>
+        <meta property="twitter:description" content="fountaintechies - We create professional Apple iOS, Google Android and HTML5 applications"/>
+        <meta property="twitter:image" content="http://www.fountaintechies.com/images/af-og-image.jpg">
+          <link rel="canonical" href="http://www.fountaintechies.com/about/">
+            <link href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,100,700" rel="stylesheet" type="text/css">
+              <link rel="stylesheet" href="./assets/main-76c23bee270ceafbda6b8c37f78298bd.css" type="text/css"/>
+              <link rel="stylesheet" href="./assets/syntax-4e68be894597df37d65b26d04a07c779.css" type="text/css"/>
+              <link rel="stylesheet" href="./assets/lightbox-d09d49f017a9d99d1dc8fa3db247490c.css" type="text/css"/>
+              <link rel="shortcut icon" href="favicon.ico">
+                <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-45832666-2', 'auto');ga('send', 'pageview');</script>
+                <script>!function(){if("-ms-user-select"in document.documentElement.style&&navigator.userAgent.match(/IEMobile\/10\.0/)){var e=document.createElement("style");e.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.getElementsByTagName("head")[0].appendChild(e)}}()</script>
+              </head>
+              <body ng-app="app" ng-class="{chrome: chrome}" class="">
+                <div id="wrap">
+                  <div class="navbar navbar-orange navbar-default navbar-fixed-top" role="navigation" animated-navigation="" ng-controller="navbarController">
+                    <div class="container" style="position:relative">
+                      <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" ng-click="collapse.open = !collapse.open">
+                          <span class="sr-only">Toggle navigation</span>
+                          <span class="icon-bar">
+                          </span>
+                          <span class="icon-bar">
+                          </span>
+                          <span class="icon-bar">
+                          </span>
+                        </button>
+                        <a class="navbar-brand is-hidden" href="../index.html" animated-navigation-logo="">
+                          <i class="navbar-brand-icon">
+                          </i>
+                          <div class="navbar-brand-text">fountaintechies</div>
+                        </a>
+                      </div>
+                      <div class="navbar-collapse collapse" ng-class="{in: collapse.open}">
+                        <ul class="nav navbar-nav navbar-right is-hidden" animated-navigation-navigation="">
+                          <li>
+                              <a class="page-link Clients" href="../nodejs/index.html">Nodejs</a>
+                          </li>
+                          <li>
+                            <a class="page-link Clients" href="../angularjs/index.html">Angularjs</a>
+                          </li>
+                          <li>
+                            <a class="page-link Clients" href="../ibeacon/index.html">Ibeacon</a>
+                          </li>                        
+                          <li>
+                            <a class="page-link About Us" href="index.html">About Us</a>
+                          </li>
+                          <li>
+                           <!--  <a class="page-link Blog" href="../blog/index.html">Blog</a> -->
+                          </li>
+                          <li>
+                            <a class="page-link Clients" href="../clients/index.html">Clients</a>
+                          </li>
+                          <li>
+                            <a class="page-link Contact" href="../contact/index.html">Contact</a>
+                          </li>
+                          <li>
+                            <a class="page-link Jobs" href="../jobs/index.html">Jobs</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <header class="page-header about-header">
+                    <div class="page-hero">
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-xs-10 col-xs-push-1 col-sm-10 col-sm-push-1 col-md-6 col-md-push-3">
+                            <div class="page-intro">
+                              <hgroup>
+                                <h1 class="page-hero-title">Home Page</h1>
+                                <h2 class="page-hero-subtitle">Home in Singapore</h2>
+                              </hgroup>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </header>
+                  <div class="light">
+                    <div class="page container">
+                      <div class="row">
+                        <div class="col-xs-10 col-xs-push-1 col-sm-10 col-sm-push-1 col-md-push-0 col-md-12" style="padding-bottom: 20px;">
+                          <article class="post-content">
+                            <section class="row">
+                              <div class="col-md-6 margin-bottom">
+                                <h2>Fountaintechies</h2>
+                                <p>We build highly scalable business solutions in the web and app world that cater to the needs of fast growing companies on or offline.
+Due to a well balanced mix of talented programmers and designers, and by combining diverse expertise in innovation management and new product development, we create products that excite and inspire, are easy to navigate, and enable businesses to accelerate growth by tapping into new markets. Our recipe for success is a holistic solution.
+
+We are providing guidance through the entire process of the development, from ideation, to deployment, working closely with our clients to be able to relate to their specific needs and shaping the product this way.
+Our young, lively yet dexterous team at Fountain Technologies works with absolute commitment - ensuring satisfied customers.</p><br/>
+                              
+
+                                <h2>Our Technologies</h2>
+                                
+                                
+
+
+                               
+                                </div>
+                    <?php
+include("./database/db.php");
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+       $nameErr = "Only letters and white space allowed"; 
+     }
+   }
+   
+   if (empty($_POST["email"])) {
+     $emailErr = "Email is required";
+   } else {
+     $email = test_input($_POST["email"]);
+     // check if e-mail address is well-formed
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Invalid email format"; 
+     }
+   }
+   if (empty($_POST["comment"])) {
+     $comment = "";
+   } else {
+     $comment = test_input($_POST["comment"]);
+   }
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
+
+                    <div class="col-md-5 col-md-push-1 margin-bottom">
+                        <h2>Contact Us</h2> 
+                            <div class="row">
+                              <div class="col-md-12">
+                                <p><span class="error">* required field.</span></p>
+                                   <div ng-controller="ExampleController">
+                                      <form novalidate class="simple-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="name">
+                                                        Name</label>
+                                                    <input type="text" class="form-control contact_input" placeholder="Enter name" ng-model="user.name" required="required" value="<?php echo $name;?>" />
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="email">
+                                                        Email Address</label>
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                                        </span>
+                                                        <input type="email" class="form-control contact_input" ng-model="user.email" placeholder="Enter email" required="required" value="<?php echo $email;?>" /></div>
+                                                </div>
+                                            </div> 
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="name">
+                                                        Brief</label>
+                                                    <textarea name="message" id="message" class="form-control" rows="4" cols="25" required="required"
+                                                        placeholder="Brief"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-12">
+                                            <button type="clear" class="btn btn-primary  save_btn" ng-click="reset()" value="Reset">
+                                                    Reset</button>
+                                            <button type="submit" class="btn btn-primary pull-right save_btn" ng-click="update(user)" value="Save">
+                                                    Save</button>
+                                            </div>
+                                        </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+
+
+
+$sql = "INSERT INTO aboutus (name, email, brief)
+VALUES ('$name', '$email', '$comment')";
+// use exec() because no results are returned
+echo "Thank You!";
+mysql_query($sql) or die(mysql_error());
+
+?>
+                                      
+                          </div>
+                    </section>
+
+                             
+                            </article>
+                            <article>
+                                 <section class="our-technology">
+                                  <div class="col-md-3 align-center">
+                                      <h2>Angularjs</h2> 
+                                      <img srcset="../images/angularjs.png" alt="Angularjs" width="200" />
+                                      <a>Read More</a>
+                                  </div>
+                                   <div class="col-md-3 align-center">
+                                      <h2>Nodejs</h2>
+                                      <img srcset="../images/nodejs_1.jpg" alt="Angularjs" width="200" /> 
+                                      <a href="../about/nodejs.html">Read More</a>
+                                  </div>
+                                   <div class="col-md-3 align-center">
+                                      <h2>PHP</h2> 
+                                      <img srcset="../images/php.png" alt="Angularjs" width="200" />
+                                      <a>Read More</a>
+                                  </div>
+                                   <div class="col-md-3 align-center">
+                                      <h2>IOT</h2> 
+                                      <img srcset="../images/iot.jpg" alt="Angularjs" width="200" />
+                                      <a>Read More</a>
+                                  </div>
+                                  
+                                </section>  
+                            </article>
+                          </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div id="push">
+                          </div>
+                        </div>
+
+                        <section id="contact" class="dark footer border-top contact xs-margin sm-margin md-margin" footer-element="">
+                          <div class="container">
+                            <div class="row">
+                              <div class="contact-col col-xs-12 col-sm-3 col-md-3 align-center">
+                                <div class="row">
+                                  <i class="contact-icon fa fa-phone">
+                                  </i>
+                                  <h3 class="contact-title">Give us a call!</h3>
+                                  <h4 class="contact-label">
+                                    <a href="tel:006586737532">+65 8673 7532</a>
+                                  </h4>
+                                </div>
+                              </div>
+                              <div class="contact-col col-xs-12 col-sm-3 col-md-3 align-center">
+                                <div class="row">
+                                  <i class="contact-icon fa fa-envelope">
+                                  </i>
+                                  <h3 class="contact-title"> Send us an email!</h3>
+                                  <h4 class="contact-label">
+                                    <a href="mailto:info@fountaintechies.com">info@fountaintechies.com</a>
+                                  </h4>
+                                </div>
+                              </div>
+                              <div class="contact-col col-xs-12 col-sm-3 col-md-3 align-center">
+                                <div class="row">
+                                  <i class="contact-icon fa fa-twitter">
+                                  </i>
+                                  <h3 class="contact-title"> Follow us!</h3>
+                                  <h4 class="contact-label">
+                                    <a href="https://twitter.com/fountaintechies" target="_blank">@fountaintechies</a>
+                                  </h4>
+                                </div>
+                              </div>
+                              <div class="contact-col col-xs-12 col-sm-3 col-md-3 align-center">
+                                <div class="row">
+                                  <i class="contact-icon fa fa-github-alt">
+                                  </i>
+                                  <h3 class="contact-title"> Fork us on GitHub!</h3>
+                                  <h4 class="contact-label">
+                                    <a href="https://github.com/fountaintechies" target="_blank">fountaintechies</a>
+                                  </h4>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </section>
+
+                        <div class="align-center dark footer border-bottom">
+                          <p class="copyright">
+                            <small>&copy; 2015 Fountain Technologies Pte. Ltd.</small>
+                          </p>
+                        </div>
+                        <script src="../assets/app-fb0e394c60653852f6abd8bed493720e.js">
+                        </script>
+                        <script src="../assets/vendor/lightbox.min-c88dcd8d658bb8192df1f50fb6d4ccba.js">
+                        </script>
+                      </body>
+                    </html>
